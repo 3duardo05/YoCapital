67bc4e44f324bfb0fbebc410220196cdd07c77a1
EDUARDO <eduuardorodriguezrdz@gmail.com>
2026-02-19 19:43:56 -0600

    Creacion del Menu de la Actividad Vendedor

---
+++ app/src/main/java/com/example/yocapital/vendedor/HomeVendedorFragment.kt
@@ -0,0 +1,20 @@
+package com.example.yocapital.vendedor // Deja el tuyo como estaba
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.yocapital.R
+
+class HomeVendedorFragment : Fragment() {
+
+    // Esta función es la única importante: es la que dibuja la pantalla
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Aquí le dice: "Infla (dibuja) el diseño de fragment_home_vendedor"
+        return inflater.inflate(R.layout.fragment_home_vendedor, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/yocapital/vendedor/VendedorActivity.kt
@@ -0,0 +1,47 @@
+package com.example.yocapital.vendedor // OJO: Deja el tuyo como esté
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.yocapital.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class VendedorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_vendedor)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_inicio -> {
+                    cambiarFragmento(HomeVendedorFragment())
+                    true
+                }
+                R.id.menu_agregar -> {
+                    cambiarFragmento(VentaVendedorFragment())
+                    true
+                }
+                R.id.menu_perfil -> {
+                    cambiarFragmento(PerfilVendedorFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+        if (savedInstanceState == null) {
+            cambiarFragmento(HomeVendedorFragment())
+            bottomNav.selectedItemId = R.id.menu_inicio
+        }
+    }
+
+    private fun cambiarFragmento(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+}
